bd24df41b7869475c56708c4ffc8be893fbfa46a
Ilya Kamens <ilyakamens@gmail.com>
2018-02-05 08:30:30 -0500

    Crappy install script

---
+++ install.zsh
@@ -0,0 +1,8 @@
+# TODO: Use soft links instead 
+
+cp Preferences.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+cp Anaconda.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Anaconda.sublime-settings
+cp Package\ Control.sublime-settings /Users/$USER/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+
+cp vimrc ~/.vimrc
+cp gitconfig ~/.gitconfig
