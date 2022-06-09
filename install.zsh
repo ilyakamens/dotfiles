e944a760f21c2c14481e145b18ad355a4a71a9b1
Ilya Kamens <ilyakamens@gmail.com>
2022-06-09 12:16:48 +0200

    Include homebrew in installation

--- install.zsh
+++ install.zsh
@@ -12,9 +12,14 @@ touch ~/.android-config
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 ln -f vimrc ~/.vimrc
 ln -f gitconfig ~/.gitconfig
 ln -f iterm-profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/iterm-profile.json
 ln -f vscode/propel/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+brew bundle
 
 . ~/.zshrc
