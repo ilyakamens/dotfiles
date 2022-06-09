3594ea181a1295134a352039c14dc20b635c3cbe
Ilya Kamens <ilyakamens@gmail.com>
2022-06-09 11:13:03 +0200

    Update install script

--- install.zsh
+++ install.zsh
@@ -1,6 +1,18 @@
 #!/usr/bin/env zsh
 
+echo ". ~/dev/dotfiles/zshrc" > ~/.zshrc
+
+# Propel
+touch ~/.secrets
+touch ~/.android-config
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 ln -f vimrc ~/.vimrc
 ln -f gitconfig ~/.gitconfig
 ln -f iterm-profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/iterm-profile.json
 ln -f vscode/propel/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+. ~/.zshrc
